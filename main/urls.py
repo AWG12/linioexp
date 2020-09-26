6cc99e76d0b13c0b4df75ff23a0ceef7dc46b495
Wong <12thonywg@gmail.com>
2020-09-26 00:58:02 -0500

    modificacion simple

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,29 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('productos', views.ProductListView.as_view(), name='product-list'),
     path('productos/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
+
+    path('proveedores', views.ProveedorListView.as_view(), name='proveedor-list'),
+    path('proveedores/<int:pk>', views.ProveedorDetailView.as_view(), name='proveedor-detail'),
+
+    path('categorias', views.CategoriaListView.as_view(), name='categorias-list'),
+    path('categorias/<int:pk>', views.CategoriaDetailView.as_view(), name='categorias-detail'),
+
+    path('localizaciones', views.LocalizacionListView.as_view(), name='localizaciones-list'),
+    path('localizaciones/<int:pk>', views.LocalizacionDetailView.as_view(), name='localizaciones-detail'),
+
+    path('detallepedido', views.DetallePedidoListView.as_view(), name='detallepedido-list'),
+    path('detallepedido/<int:pk>', views.DetallePedidoDetailView.as_view(), name='detallepedido-detail'),
+
+    path('pedidos', views.PedidoListView.as_view(), name='pedido-list'),
+    path('pedidos/<int:pk>', views.PedidoDetailView.as_view(), name='pedido-detail'),
+
+    path('usuarios', views.UsuarioListView.as_view(), name='usuario-list'),
+    path('usuarios/<int:pk>', views.UsuarioDetailView.as_view(), name='usuario-detail'),
+
+    path('clientes', views.ClienteListView.as_view(), name='clientes-list'),
+    path('clientes/<int:pk>', views.ClienteDetailView.as_view(), name='clientes-detail'),
+
+    path('colaboradores', views.ColaboradorListView.as_view(), name='colaborador-list'),
+    path('colaboradores/<int:pk>', views.ColaboradorDetailView.as_view(), name='colaborador-detail'),
 ]
 
--- main/views.py
+++ main/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 from django.views.generic import ListView, DetailView
 
-from main.models import Producto
+from main.models import Producto, Proveedor, Categoria, DetallePedido, Usuario, Cliente, Colaborador, Localizacion, Pedido
 
 def home(request):
   return HttpResponse("Hola Mundo. Te encuentras en la página de inicio del Linio Express")
@@ -15,3 +15,60 @@ class ProductListView(ListView):
 
 class ProductDetailView(DetailView):
     model = Producto
+
+
+class LocalizacionListView(ListView):
+    model = Localizacion
+
+class LocalizacionDetailView(DetailView):
+    model = Localizacion
+
+
+class DetallePedidoListView(ListView):
+    model = DetallePedido
+
+class DetallePedidoDetailView(DetailView):
+    model = DetallePedido
+
+
+class PedidoListView(ListView):
+    model = Pedido
+
+class PedidoDetailView(DetailView):
+    model = Pedido
+
+
+class UsuarioListView(ListView):
+    model = Usuario
+
+class UsuarioDetailView(DetailView):
+    model = Usuario
+
+
+class ClienteListView(ListView):
+    model = Cliente
+
+class ClienteDetailView(DetailView):
+    model = Cliente
+
+
+class ColaboradorListView(ListView):
+    model = Colaborador
+
+class ColaboradorDetailView(DetailView):
+    model = Colaborador
+
+
+class ProveedorListView(ListView):
+    model = Proveedor
+
+class ProveedorDetailView(DetailView):
+    model = Proveedor
+
+
+class CategoriaListView(ListView):
+    model = Categoria
+
+class CategoriaDetailView(DetailView):
+    model = Categoria
+
